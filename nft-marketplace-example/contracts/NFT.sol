6e7c2ecd56fcd1a4896f0ad0af28065babe9b9cd
dabit3 <dabit3@gmail.com>
2021-06-19 14:34:16 -0500

    initial commit

---
+++ nft-marketplace-example/contracts/NFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Eat The Blocks NFTs", "ETBNFT") {}
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
+}
---
+++ nft-marketplace-example/test/test.js
@@ -0,0 +1,63 @@
+const { BigNumber } = ethers
+const axios = require('axios')
+
+describe("NFTMarket", function() {
+  it("Should interact with the token contract", async function() {
+
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy();
+    await nft.deployed()
+    const nftContractAddress = nft.address;
+
+    const Market = await ethers.getContractFactory("NFTMarket");
+    const market = await Market.deploy();
+    await market.deployed()
+    const address = market.address; 
+
+    let transaction = await nft.createToken("https://ipfs.fleek.co/ipfs/bafybeicfghmsa3qukuz3dl4cwsoe7muscoaym5z6jhsxdlxwguun43cpfy")
+    
+    let tx = await transaction.wait()
+    let event = tx.events.pop()
+    let value = event.args[2]
+    value = BigNumber.from(value).toNumber()
+    console.log('value: ', value)
+
+    transaction = await nft.createToken("https://ipfs.fleek.co/ipfs/bafybeicfghmsa3qukuz3dl4cwsoe7muscoaym5z6jhsxdlxwguun43cpfy")
+
+    tx = await transaction.wait()
+    event = tx.events.pop()
+    value = event.args[2]
+    value = BigNumber.from(value).toNumber()
+    console.log('value: ', value)
+
+    transaction = await nft.createToken("https://ipfs.fleek.co/ipfs/bafybeicfghmsa3qukuz3dl4cwsoe7muscoaym5z6jhsxdlxwguun43cpfy")
+
+    await nft.createToken("https://ipfs.fleek.co/ipfs/bafybeicfghmsa3qukuz3dl4cwsoe7muscoaym5z6jhsxdlxwguun43cpfy")
+
+    await nft.setApprovalForAll(address, true)
+  
+    await market.createMarketItem(nftContractAddress, 1, 1000)
+    await market.createMarketItem(nftContractAddress, 2, 1000)
+    await market.createMarketItem(nftContractAddress, 3, 1000)
+
+    const [_, userAddress, userAddress2] = await ethers.getSigners();
+
+    await market.connect(userAddress).createMarketSale(nftContractAddress, 1, { value: 1000})
+    await market.connect(userAddress2).createMarketSale(nftContractAddress, 2, { value: 1000})
+
+    let items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      const tokenMeta = await axios.get(tokenUri)
+      console.log('tokenMeta: ', tokenMeta.data)
+      let item = {
+        price: BigNumber.from(i.price).toNumber(),
+        tokenId: BigNumber.from(i.tokenId).toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+  });
+});
